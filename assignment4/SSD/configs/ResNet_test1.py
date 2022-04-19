4da3054d2126808e67f004f50ab41231b37a143a
rambech <thatalexram@gmail.com>
2022-04-19 15:15:01 +0200

    FPN backbone done and runs! We had to put a max pool after layer1 and change the adaptive average pool of ResNet in order for the shapes of the features to fit. All in all this is a pretty good implementation. Although the grads goes to zero indicating that we need to implement the weights using trick 4

--- assignment4/SSD/configs/ResNet_test1.py
+++ assignment4/SSD/configs/ResNet_test1.py
@@ -1,9 +1,7 @@
-import torchvision
-import torch
 from tops.config import LazyCall as L
 from ssd.modeling import backbones
-from .tdt4265 import train, anchors, optimizer, schedulers, model, data_train, data_val, loss_objective, label_map
+from .tdt4265 import train, anchors, optimizer, schedulers, model, data_train, data_val, loss_objective, label_map, train_cpu_transform, val_cpu_transform, gpu_transform
 
 train["epochs"] = 50
 
-backbone = L(backbones.NewFPN)()
+backbone = L(backbones.FPN)(out_channels = [256, 256, 256, 256, 256, 256])
--- assignment4/SSD/ssd/modeling/backbones/fpn.py
+++ assignment4/SSD/ssd/modeling/backbones/fpn.py
@@ -1,124 +1,66 @@
-'''FPN in PyTorch.
-See the paper "Feature Pyramid Networks for Object Detection" for more details.
-'''
-import torch
+from typing import OrderedDict
 import torch.nn as nn
-import torch.nn.functional as F
-
-from torch.autograd import Variable
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
+import torchvision.ops as ops
+import torchvision.models as models
+from typing import Tuple, List
 
 class FPN(nn.Module):
-    def __init__(self, block, num_blocks):
-        super(FPN, self).__init__()
-        self.in_planes = 64
+    """
+    A new backbone consisting of ResNet-50 with and FPN on top.
+    The FPN is made using pytorch helper functions
+    
+    """
+    
+    def __init__(self, out_channels):
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-
-        # Bottom-up layers
-        self.layer1 = self._make_layer(block,  64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-
-        # Top layer
-        self.toplayer = nn.Conv2d(2048, 256, kernel_size=1, stride=1, padding=0)  # Reduce channels
-
-        # Smooth layers
-        self.smooth1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.smooth2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.smooth3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-
-        # Lateral layers
-        self.latlayer1 = nn.Conv2d(1024, 256, kernel_size=1, stride=1, padding=0)
-        self.latlayer2 = nn.Conv2d( 512, 256, kernel_size=1, stride=1, padding=0)
-        self.latlayer3 = nn.Conv2d( 256, 256, kernel_size=1, stride=1, padding=0)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def _upsample_add(self, x, y):
-        '''Upsample and add two feature maps.
-        Args:
-          x: (Variable) top feature map to be upsampled.
-          y: (Variable) lateral feature map.
-        Returns:
-          (Variable) added feature map.
-        Note in PyTorch, when input size is odd, the upsampled feature map
-        with `F.upsample(..., scale_factor=2, mode='nearest')`
-        maybe not equal to the lateral feature map size.
-        e.g.
-        original input size: [N,_,15,15] ->
-        conv2d feature map size: [N,_,8,8] ->
-        upsampled feature map size: [N,_,16,16]
-        So we choose bilinear upsample which supports arbitrary output sizes.
-        '''
-        _,_,H,W = y.size()
-        return F.upsample(x, size=(H,W), mode='bilinear') + y
+        super().__init__()
+        
+        self.model = models.resnet50(pretrained=True)
+        self.model.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 8)) #default for ResNet is output_size=(1, 1)
+        self.out_channels = out_channels
+        self.fpn = ops.FeaturePyramidNetwork(in_channels_list =[64, 256, 512, 1024, 2048], out_channels = self.out_channels[0])
 
     def forward(self, x):
-        # Bottom-up
-        c1 = F.relu(self.bn1(self.conv1(x)))
-        c1 = F.max_pool2d(c1, kernel_size=3, stride=2, padding=1)
-        c2 = self.layer1(c1)
-        c3 = self.layer2(c2)
-        c4 = self.layer3(c3)
-        c5 = self.layer4(c4)
-        # Top-down
-        p5 = self.toplayer(c5)
-        p4 = self._upsample_add(p5, self.latlayer1(c4))
-        p3 = self._upsample_add(p4, self.latlayer2(c3))
-        p2 = self._upsample_add(p3, self.latlayer3(c2))
-        # Smooth
-        p4 = self.smooth1(p4)
-        p3 = self.smooth2(p3)
-        p2 = self.smooth3(p2)
-        return p2, p3, p4, p5
+        feature_map_dict = OrderedDict()
 
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+        feature_map_dict["feat0"] = x
 
-def FPN101():
-    # return FPN(Bottleneck, [2,4,23,3])
-    return FPN(Bottleneck, [2,2,2,2])
+        x = self.model.layer1(x)
+        x = self.model.maxpool(x)
+        feature_map_dict[f"feat1"] = x
 
+        x = self.model.layer2(x)
+        feature_map_dict[f"feat2"] = x
 
-def test():
-    net = FPN101()
-    fms = net(Variable(torch.randn(1,3,600,900)))
-    for fm in fms:
-        print(fm.size())
+        x = self.model.layer3(x)
+        feature_map_dict[f"feat3"] = x
 
-test()
+        x = self.model.layer4(x)
+        feature_map_dict[f"feat4"] = x
+        
+        x = self.model.avgpool(x)
+        feature_map_dict[f"feat5"] = x
+
+        output = self.fpn(feature_map_dict)
+        outputs = output.values()
+        out_features = list(outputs)
+        # print(f"Printing model: {self.model}")
+        # print(f"Out_features: {out_features}")
+        # print("Printing features")
+        # print([(k, v.shape) for k, v in output.items()])
+            
+        return out_features
+        
+    
+def main():
+    ResNet = models.resnet50(pretrained=True)
+    ResNet.eval()
+    print(ResNet)
+    
+
+if __name__ == "__main__":
+    main()

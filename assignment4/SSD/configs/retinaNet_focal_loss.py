31541f0ac65acbc89ffd4ec0b3db540001ec9398
Alexander Brevad Rambech <alexaram@clab11.idi.ntnu.no>
2022-05-03 11:06:35 +0200

    Updated subnet config to easier compare subnets and shared heads models

--- assignment4/SSD/configs/retinaNet_focal_loss.py
+++ assignment4/SSD/configs/retinaNet_focal_loss.py
@@ -2,6 +2,8 @@ from tops.config import LazyCall as L
 from ssd.modeling.ssd_multibox_loss import RetinaFocalLoss 
 from .retinaNet_FPN import train, anchors, optimizer, schedulers, model, backbone, data_train, data_val, label_map, train_cpu_transform, val_cpu_transform, gpu_transform
 
-loss_objective = L(RetinaFocalLoss)(anchors="${anchors}")
+alpha = [0.01, 1, 1, 1, 1, 1, 1, 1, 1]  # Default alpha from project assignment
+
+loss_objective = L(RetinaFocalLoss)(anchors="${anchors}", alpha = alpha)
 
 # class_names = ("background", "car", "truck", "bus", "motorcycle", "bicycle", "scooter", "person", "rider")

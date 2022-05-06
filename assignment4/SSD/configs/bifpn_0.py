46b4b8627ca388749278ca597b6d1a86c9832776
Alexander B. Rambech <alexander.rambech@gmail.com>
2022-05-06 19:17:12 +0200

    bugfix

--- assignment4/SSD/configs/bifpn_0.py
+++ assignment4/SSD/configs/bifpn_0.py
@@ -13,7 +13,8 @@ anchors = L(AnchorBoxes)(
     # aspect ratio is used to define two boxes per element in the list.
     # if ratio=[2], boxes will be created with ratio 1:2 and 2:1
     # Number of boxes per location is in total 2 + 2 per aspect ratio
-    aspect_ratios=[[2, 3], [2, 3], [2, 5], [2, 5], [2, 3], [2, 3]], #Original
+    # aspect_ratios=[[2, 3], [2, 3], [2, 5], [2, 5], [2, 3], [2, 3]], #Original
+    aspect_ratios=[[2, 3], [2, 3], [2, 3], [2, 3], [2, 3], [2, 3]],
     image_shape="${train.imshape}",
     scale_center_variance=0.1,
     scale_size_variance=0.2
@@ -21,11 +22,12 @@ anchors = L(AnchorBoxes)(
 
 backbone = L(BiFPN)(
     phi=0, 
-    #image_channels="${train.image_channels}",
     output_feature_sizes="${anchors.feature_sizes}"
     )
 
-alpha = [0.01, 1, 10, 10, 10, 10, 10, 3, 5]
+# alpha = [0.01, 1, 10, 10, 10, 10, 10, 3, 5]
+alpha = [0.01, 1, 1, 1, 1, 1, 1, 1, 1]
+
 loss_objective = L(RetinaFocalLoss)(anchors=anchors, alpha=alpha)
 
 model = L(RetinaNetSharedHeads)(

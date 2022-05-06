7afa3215ccc41f93b8e161dbd6deaf84f3c9593d
Alexander B. Rambech <alexander.rambech@gmail.com>
2022-05-06 13:17:28 +0200

    bugfix

--- assignment4/SSD/ssd/modeling/backbones/bifpn.py
+++ assignment4/SSD/ssd/modeling/backbones/bifpn.py
@@ -16,7 +16,7 @@ class BiFPN(nn.Module):
         # Choosing the baseline EfficientNet as backbone network
         self.model = EfficientNet(phi)
 
-        self.images_channels = images_channels
+        self.image_channels = images_channels
         self.output_feature_sizes = output_feature_sizes
         self.num_features = 6 # Number to take out of the BiFPN, is 5 in the paper, but 6 is chosen here
         self.out_channels = [256, 256, 256, 256, 256, 256]

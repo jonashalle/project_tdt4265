86d6a328f6525914c3cdb56112dda3057c1ec181
Alexander B. Rambech <alexander.rambech@gmail.com>
2022-05-07 12:39:42 +0200

    bugfix

--- assignment4/SSD/configs/bifpn_1.py
+++ assignment4/SSD/configs/bifpn_1.py
@@ -4,8 +4,4 @@ from tops.config import LazyCall as L
 from .bifpn_0 import model, anchors, loss_objective, backbone, train, optimizer, schedulers, data_train, data_val, label_map, train_cpu_transform, val_cpu_transform, gpu_transform
 
 
-backbone = L(EfficientNet)(
-    phi=0, 
-    # output_feature_sizes="${anchors.feature_sizes}"
-    )
-model.feature_extractor = backbone
+backbone.phi = 2

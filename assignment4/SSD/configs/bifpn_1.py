f15a15259f7614b86001ce1d83f4670f409ee415
Alexander B. Rambech <alexander.rambech@gmail.com>
2022-05-07 12:50:52 +0200

    phi = 4

--- assignment4/SSD/configs/bifpn_1.py
+++ assignment4/SSD/configs/bifpn_1.py
@@ -4,4 +4,4 @@ from tops.config import LazyCall as L
 from .bifpn_0 import model, anchors, loss_objective, backbone, train, optimizer, schedulers, data_train, data_val, label_map, train_cpu_transform, val_cpu_transform, gpu_transform
 
 
-backbone.phi = 4
+backbone.phi = 5
